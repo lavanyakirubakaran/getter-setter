9f9ce1d0b02443b6be70d1d5248833f91e883381
lavanyarajendran <116621788+lavanyakirubakaran@users.noreply.github.com>
2022-11-18 09:37:09 +0530

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,50 @@
+package com.animals;
+
+public class Animals {
+	public String name;
+	public int batch;
+	public String color;
+	public String pob;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBatch() {
+		return batch;
+	}
+	public void setBatch(int batch) {
+		this.batch = batch;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getPob() {
+		return pob;
+	}
+	public void setPob(String pob) {
+		this.pob = pob;
+	}
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	
+	
+}
---
+++ Animalsdetails.java
@@ -0,0 +1,17 @@
+package com.animals;
+
+public class Animalsdetails {
+	public static void main(String lava[]) {
+		Animals animals=new Animals();
+		animals.setName("rajapaliyam");
+		System.out.println(animals.getName());
+		animals.setBatch(101);
+		System.out.println(animals.getBatch());
+		animals.setColor("blue");
+		System.out.println(animals.getColor());
+		animals.setPob("kalpakkam");
+		System.out.println(animals.getPob());
+
+	}
+
+}
